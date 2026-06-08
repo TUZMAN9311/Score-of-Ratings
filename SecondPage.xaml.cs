c90795d2e5fda9eafbf4d21435e9902eacad9022
TUZMAN9311 <voroshilin-m@mail.ru>
2026-06-08 21:34:56 +0300

    Fixed a problem with links

--- SecondPage.xaml.cs
+++ SecondPage.xaml.cs
@@ -8,18 +8,46 @@ public partial class SecondPage : ContentPage
     }
     public async void YoutubeButtonClicked(object sender, EventArgs e)
     {
+        try
+        {
         await Launcher.Default.OpenAsync("https://www.youtube.com/@TUZMAN9311");
+        }
+        catch(Exception ex)
+        {
+            await DisplayAlert("Упс!", "Не удалось открыть ссылку. Похоже, у вас в системе отключен или отсутствует браузер.", "ОК");
+        }
     }
     public async void TwitchButtonClicked(object sender, EventArgs e)
     {
-        await Launcher.Default.OpenAsync("https://www.twitch.tv/tuzman9311");
+        try
+        {
+            await Launcher.Default.OpenAsync("https://www.twitch.tv/tuzman9311");
+        }
+        catch(Exception ex)
+        {
+            await DisplayAlert("Упс!", "Не удалось открыть ссылку. Похоже, у вас в системе отключен или отсутствует браузер.", "ОК");
+        }
     }
     public async void KickButtonClicked(object sender, EventArgs e)
     {
-        await Launcher.Default.OpenAsync("https://kick.com/tuzman9311");
+        try
+        {
+            await Launcher.Default.OpenAsync("https://kick.com/tuzman9311");
+        }
+        catch(Exception ex)
+        {
+            await DisplayAlert("Упс!", "Не удалось открыть ссылку. Похоже, у вас в системе отключен или отсутствует браузер.", "ОК");
+        }
     }
     public async void GitHubButtonClicked(object sender, EventArgs e)
     {
-        await Launcher.Default.OpenAsync("https://github.com/TUZMAN9311");
+        try
+        {
+            await Launcher.Default.OpenAsync("https://github.com/TUZMAN9311");
+        }
+        catch(Exception ex)
+        {
+            await DisplayAlert("Упс!", "Не удалось открыть ссылку. Похоже, у вас в системе отключен или отсутствует браузер.", "ОК");
+        }
     }
 }
